bff9d390bf48c1d2217f1b85615596c3ae9ee1e3
tkchathura <34586010+tkchathura@users.noreply.github.com>
2019-11-08 23:56:33 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,121 @@
 function initMap() {
-
-  var styledMapType = new google.maps.StyledMapType(
-    [
-      {
-        "featureType": "all",
-        "elementType": "labels",
-        "stylers": [
-          { "visibility": "off" }
-        ]
-      }
-    ],
-    {name: 'Styled Map'});
+var styledMapType = new google.maps.StyledMapType(
+        [
+        {
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.neighborhood",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.business",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.medical",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.school",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        }
+      ],
+{name: 'Styled Map'});
   
   
-     var map = new google.maps.Map(document.getElementById('map'), {
+          var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -37.797787, lng: 144.964162},
-          zoom: 14,
+          zoom: 11,
           mapTypeControlOptions: {
             mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
                     'styled_map']
           }
         });
-  
-     map.mapTypes.set('styled_map', styledMapType);
-        map.setMapTypeId('styled_map');
-      }
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    label:'1',
-    title: 'Hello World!'
-  });
+        //Associate the styled map with the MapTypeId and set it to display.
+        map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
+
+//   var marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     label:'1',
+//     title: 'Hello World!'
+//   });
 }
