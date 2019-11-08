272f9142414d26d059e97e6805e95498f0b50d96
tkchathura <34586010+tkchathura@users.noreply.github.com>
2019-11-08 23:10:54 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
-  console.log("Hi");
+  var myLatLng = {lat: -37.797787, lng:  144.964162};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 14,
     center: myLatLng
   });
 
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
+    label:'1',
     title: 'Hello World!'
   });
 }
