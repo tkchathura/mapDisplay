3a498f64cb1396f07e0d3ceb2786182c1bdd81ab
tkchathura <34586010+tkchathura@users.noreply.github.com>
2019-11-11 10:07:19 +1100

    Update index.js

--- index.js
+++ index.js
@@ -227,7 +227,7 @@ function initMap() {
       scaledSize: new google.maps.Size(20, 20),
       origin: new google.maps.Point(0, 0),
       anchor: new google.maps.Point(0,30),
-      labelOrigin:  new google.maps.Point(-5,5),
+      labelOrigin:  new google.maps.Point(5,5),
     };
 
     let markerLabelTwo = markerLabelOne+index;
